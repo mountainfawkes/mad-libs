7e124178304583c778f4c55f1baa106b78e5e3f1
cass1618 <cass16180@gmail.com>
2021-03-29 11:40:49 -0700

    Create array with each item
    Co-authored-by: Jonathan Stull <jonathan.d.stull@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val()
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+    event.preventDefault();
+    
+    let things = [];
+    
+    things.push($("input#person1").val());
+    things.push($("input#person2").val());
+    things.push($("input#animal").val());
+    things.push($("input#exclamation").val());
+    things.push($("input#verb").val());
+    things.push($("input#noun").val());
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    console.log(things);
+
+    // $(".person1").text(person1Input);
+
 
     $("#story").show();
 
